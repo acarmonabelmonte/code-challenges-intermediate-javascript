36c68d6c3bbbb3d5e758483b2342bacf8eef91e9
Adrián Carmona <acarmona.dev@gmail.com>
2020-12-11 10:40:48 +0100

    finished reverse array challenge

--- reverseArray/reverseArray.js
+++ reverseArray/reverseArray.js
@@ -5,22 +5,17 @@
 
 const reverseArray = (arr) => {
     let reversedArray = [];
-    for (let i = arr.length - 1; i >= 0; i--){
+    for (let i = arr.length - 1; i >= 0; i--) {
         reversedArray.push(arr[i]);
     }
     return reversedArray;
-}
-
-
-
-
-
+};
 
 // When you're ready to test your code, uncomment the below and run:
 
 const sentence = ['sense.','make', 'all', 'will', 'This'];
 
-console.log(reverseArray(sentence))
+console.log(reverseArray(sentence));
 
 // Should print['This', 'will', 'all', 'make', 'sense.'];
 

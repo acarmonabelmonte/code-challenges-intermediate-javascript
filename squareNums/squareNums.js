17cbab61381666190ee0db8dd1edb927ea4efd25
Adrián Carmona <acarmona.dev@gmail.com>
2020-12-11 13:36:43 +0100

    finished squeare nums code challenge

---
+++ squareNums/squareNums.js
@@ -0,0 +1,10 @@
+/* Write a function, squareNums(), that takes in an array of numbers and, using .map(), returns an array with the square of each of the elements of that array.
+You can test your function when you’re ready by passing in the numbers array or by making your own array! */
+
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const squareNums = (arr) => {
+    return arr.map(number => number ** 2);
+};
+
+console.log(squareNums(numbers))
